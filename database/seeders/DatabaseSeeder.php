7d278ab27dd8f23a5df3a68f12eddb86ccc266ac
Демьянчик Андрей Павлович (demyanchik_ap) <demyanchik_ap@nlmk.com>
2024-03-04 21:00:43 +0300

    Add database migrations

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Client;
+use App\Models\Malfunction;
+use App\Models\Ride;
+use App\Models\RideRouteData;
+use App\Models\Scooter;
+use App\Models\ScooterMalfunction;
+use Database\Factories\MalfunctionFactory;
+use Database\Factories\RideRouteDataFactory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // DB::table('clients')->truncate();
+
+        Client::factory(40)->create();
+        Scooter::factory(40)->create();
+        Ride::factory(40)->create();
+        Malfunction::factory(40)->create();
+        RideRouteData::factory(40)->create();
+        ScooterMalfunction::factory(40)->create();
+
+
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
